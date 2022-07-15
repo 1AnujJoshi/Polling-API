06252a0f3491e0e37f11fec352ba728411d50a76
Anuj Joshi <101502385+1AnujJoshi@users.noreply.github.com>
2022-07-15 12:18:50 +0530

    Update option.js

--- routes/api/v1/option.js
+++ routes/api/v1/option.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const optionApi = require('../../../controllers/api/v1/option_api');
 
-router.get('/:id/delete', optionApi.destroy);
+router.delete('/:id/delete', optionApi.destroy);
 router.post('/:id/add_vote', optionApi.addVote);
 
 module.exports = router;
